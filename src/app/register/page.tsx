ab503fb2cf8a9f973a19ba46a77e67abf09a7691
syarif <syarif.khalid@gmail.com>
2025-04-03 20:21:23 +0700

    update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,18 @@
-export default function Register(){
-    return(
-        <div>halo</div>
-    )
+import Image from "next/image";
+import ImgRegist from "@/assets/images/register.jpeg"; // Perbaiki path import
+
+export default function Register() {
+    return (
+        <div className="h-screen w-full flex">
+            {/* Menggunakan next/image untuk optimasi gambar */}
+            <div className="relative w-1/4 h-full">
+                <Image 
+                    src={ImgRegist} 
+                    alt="Register Background" 
+                    layout="fill" 
+                    objectFit="cover" 
+                />
+            </div>
+        </div>
+    );
 }
